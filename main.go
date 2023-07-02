0ce1e8f230fc10926f9d7517578720c938ca8bfb
Catboi <90759767+Mardanis@users.noreply.github.com>
2023-07-02 22:42:12 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -9,96 +9,117 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/a-17-1-penggunaan-map", func(c echo.Context) error {
-
-		chicken := make(map[string]int) // Menggunakan make() untuk membuat map
-
-		chicken["januari"] = 50
-		chicken["februari"] = 40
-
-		fmt.Println("januari", chicken["januari"]) // januari 50
-		fmt.Println("mei", chicken["mei"])         // mei 0
-
-		return c.String(http.StatusOK, "Success") // Menambahkan return statement
+	e.GET("/a-16-1-inisialisasi-slice", func(c echo.Context) error {
+		fruits := []string{"apple", "grape", "banana", "melon"}
+		return c.String(http.StatusOK, fruits[0])
 	})
 
-	e.GET("/a-17-2-inisialisasi-nilai-map", func(c echo.Context) error {
+	e.GET("/a-16-2-hubungan-array-slice-dengan-array-&-operasi-slice", func(d echo.Context) error {
+		fruits := []string{"apple", "grape", "banana", "melon"}
+		newFruits := fruits[0:2]
 
-		// cara horizontal
-		chicken1 := map[string]int{"januari": 50, "februari": 40}
-
-		// cara vertikal
-		chicken2 := map[string]int{
-			"januari":  50,
-			"februari": 40,
-		}
-
-		fmt.Println("januari", chicken1["januari"])   // januari 50
-		fmt.Println("februari", chicken1["februari"]) // februari 40
-
-		fmt.Println("januari", chicken2["januari"])   // januari 50
-		fmt.Println("februari", chicken2["februari"]) // februari 40
-
-		return c.String(http.StatusOK, "Success")
+		return d.String(200, newFruits[0])
 	})
 
-	e.GET("/a-17-3-iterasi-item-map-Menggunakan-for-range", func(e echo.Context) error {
+	e.GET("/a-16-3-merupakan-tipe-data-refference", func(e echo.Context) error {
 
-		chicken := map[string]int{
-			"januari":  50,
-			"februari": 40,
-			"maret":    34,
-			"april":    67,
-		}
+		var fruits = []string{"apple", "grape", "banana", "melon"}
 
-		for key, val := range chicken {
-			fmt.Println(key, "\t:", val)
-		}
+		aFruits := fruits[0:3]
+		bFruits := fruits[1:4]
 
-		return e.String(http.StatusOK, "Success")
+		aaFruits := aFruits[1:2]
+		baFruits := bFruits[0:1]
+
+		fmt.Println(fruits)   // [apple grape banana melon]
+		fmt.Println(aFruits)  // [apple grape banana]
+		fmt.Println(bFruits)  // [grape banana melon]
+		fmt.Println(aaFruits) // [grape]
+		fmt.Println(baFruits) // [grape]
+
+		// Buah "grape" diubah menjadi "pinnaple"
+		baFruits[0] = "pinnaple"
+
+		fmt.Println(fruits)   // [apple pinnaple banana melon]
+		fmt.Println(aFruits)  // [apple pinnaple banana]
+		fmt.Println(bFruits)  // [pinnaple banana melon]
+		fmt.Println(aaFruits) // [pinnaple]
+		fmt.Println(baFruits) // [pinnaple]
+
+		return e.String(http.StatusOK, baFruits[0])
 	})
 
-	e.GET("/a-17-4-menghapus-item-map", func(f echo.Context) error {
+	e.GET("/a-16-4fungsi-len()", func(f echo.Context) error {
 
-		var chicken = map[string]int{"januari": 50, "februari": 40}
+		var fruits = []string{"apple", "grape", "banana", "melon"}
 
-		fmt.Println(len(chicken)) // 2
-		fmt.Println(chicken)
+		return f.String(200, fruits[1]) // 4
 
-		delete(chicken, "januari")
-
-		fmt.Println(len(chicken)) // 1
-		fmt.Println(chicken)
-
-		return f.String(http.StatusOK, "Success")
 	})
 
-	e.GET("/a-17-5-deteksi-keberadaan-item-dengan-key-tertentu", func(g echo.Context) error {
+	e.GET("/a-16-5fungsi-cap()", func(g echo.Context) error {
 
-		var chicken = map[string]int{"januari": 50, "februari": 40}
-		var value, isExist = chicken["mei"]
+		var fruits = []string{"apple", "grape", "banana", "melon"}
+		fmt.Println(len(fruits)) // len: 4
+		fmt.Println(cap(fruits)) // cap: 4
 
-		if isExist {
-			fmt.Println(value)
-		} else {
-			fmt.Println("item is not exists")
-		}
+		var aFruits = fruits[0:3]
+		fmt.Println(len(aFruits)) // len: 3
+		fmt.Println(cap(aFruits)) // cap: 4
 
-		return g.String(http.StatusOK, "Success")
+		var bFruits = fruits[1:4]
+		fmt.Println(len(bFruits))        // len: 3
+		fmt.Println(cap(bFruits))        // cap: 3
+		return g.String(200, aFruits[1]) // 4
 	})
 
-	e.GET("a-17-6-kombinasi-slice&map", func(h echo.Context) error {
+	e.GET("/a-16-6-fungsi-append()", func(f echo.Context) error {
 
-		var chickens = []map[string]string{
-			map[string]string{"name": "chicken blue", "gender": "male"},
-			map[string]string{"name": "chicken red", "gender": "male"},
-			map[string]string{"name": "chicken yellow", "gender": "female"},
-		}
+		var fruits = []string{"apple", "grape", "banana"}
+		var cFruits = append(fruits, "papaya")
 
-		for _, chicken := range chickens {
-			fmt.Println(chicken["gender"], chicken["name"])
-		}
-		return h.String(http.StatusOK, "Success")
+		fmt.Println(fruits)  // ["apple", "grape", "banana"]
+		fmt.Println(cFruits) // ["apple", "grape", "banana", "papaya"]}
+
+		return f.String(200, cFruits[0]) // 4
+
 	})
+
+	e.GET("/a-16-7-fungsi-copy()", func(f echo.Context) error {
+
+		dst := []string{"potato", "potato", "potato"}
+		src := []string{"watermelon", "pinnaple"}
+		n := copy(dst, src)
+
+		fmt.Println(dst) // watermelon pinnaple potato
+		fmt.Println(src) // watermelon pinnaple
+		fmt.Println(n)   // 2
+
+		return f.String(200, dst[0]) // 4
+
+	})
+
+	e.GET("/a-16-8-pengaksesan-elemen-slice-dengan-3-ndeks", func(f echo.Context) error {
+
+		var fruits = []string{"apple", "grape", "banana"}
+		var aFruits = fruits[0:2]
+		var bFruits = fruits[0:2:2]
+
+		fmt.Println(fruits)      // ["apple", "grape", "banana"]
+		fmt.Println(len(fruits)) // len: 3
+		fmt.Println(cap(fruits)) // cap: 3
+
+		fmt.Println(aFruits)      // ["apple", "grape"]
+		fmt.Println(len(aFruits)) // len: 2
+		fmt.Println(cap(aFruits)) // cap: 3
+
+		fmt.Println(bFruits)      // ["apple", "grape"]
+		fmt.Println(len(bFruits)) // len: 2
+		fmt.Println(cap(bFruits)) // cap: 2
+
+		return f.String(200, fruits[0]) // 4
+
+	})
+
 	e.Start(":9000")
 }
